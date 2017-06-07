c7d2408ff24ca57c45ccd93f36271bf1b04dbfdf
Dalex Tanguy <dalex.tanguy@gmail.com>
2017-06-07 10:05:44 +0200

    question 1 + 2

---
+++ index.php
@@ -0,0 +1,28 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>Data Vizualisation - TP1</title>
+		<!-- Inclusion CSS (librairie + perso) -->
+		<link rel="stylesheet" type="text/css" href="css/jquery.jqplot.min.css">
+		<link rel="stylesheet" type="text/css" href="css/dataviz.css">
+		
+		<!-- Inclusion JS (librairie + scripts de création de graph) -->
+		<script type="text/javascript" src="js/jquery.js"></script>
+		<script type="text/javascript" src="js/jquery.jqplot.min.js"></script>
+		<script type="text/javascript" src="js/d3.min.js"></script>
+		
+		<script type="text/javascript"> const user = <?php echo $_GET['user']; ?></script>
+		<script type="text/javascript" src="js/dataviz.js"></script>
+	</head>
+	<body>
+		<?php include ('structure/header.php'); ?>
+		<div id="content">
+			<div class="listeAmis" id="bar_chart_random_values">
+				<h1>Evolution du nombre d’amis au fil du mois (en noir) </h1>
+				<h1>Evolution de la popularité au fil du mois (en rouge)</h1>
+				<div id="listeAmis"></div>
+			</div>
+		</div>
+		<?php include ('structure/footer.php'); ?>
+	</body>
+</html>
---
+++ js/dataviz.js
@@ -0,0 +1,154 @@
+﻿$(document).ready(function(){
+	// Pas de cache sur les requête IMPORTANT !
+	$.ajaxSetup({ cache: false });
+	
+	/*** 
+		On définit ici les fonctions de base qui vont nous servir à la récupération des données
+		Je ne définis que le GET ici, mais il est possible d'utiliser POST pour récupérer ses données (on le verra dans un prochain TP)
+	****/
+	function getRequest(url, callback) {
+		$.get(url, function(data) {
+			data = $.parseJSON(data);
+			callback(data);
+		});
+	}
+	
+	var h = 300;
+	var w = 800;		
+	
+	function generateListeAmis(url, idDivToFill) { 
+		getRequest(url, function(dataset) {
+			var xScale = d3.scale.linear()
+				.domain([0, 35])
+				.range([0, w]);
+				
+			var yScale = d3.scale.linear()
+				.domain([0, d3.max(dataset, function(d) {return d[1]+5;})])
+				.range([h-24, 0]);
+			
+			var xAxis = d3.svg.axis()
+				.scale(xScale)
+				.orient('bottom');
+				
+			var yAxis = d3.svg.axis()
+				.scale(yScale)
+				.orient('left');
+				
+			var	svg = d3.select("#"+idDivToFill)
+			.append("svg")
+			.attr('height', h)
+			.attr('width', w);
+			
+			svg
+			.selectAll("circle")
+			.data(dataset)
+			.enter()	
+			.append("circle")
+			.attr('cx', function(d){ return xScale(parseInt(d[2].substring(8,11))+1);})
+			.attr('cy', function(d){ return yScale(d[1]);})
+			.attr('r', 5);
+			
+			svg
+			.selectAll("text")
+			.data(dataset)
+			.enter()	
+			.append("text")
+			.text(function(d){
+				return d[1] + ', ' + d[2];
+			})
+			.attr('x', function(d){ return xScale(parseInt(d[2].substring(8,11)));})
+			.attr('y', function(d){ return yScale(d[1]-3);});
+			
+			svg
+			.append("text")
+			.attr("class", "x label")
+			.attr("text-anchor", "end")
+			.attr("x", w -10)
+			.attr("y", h - 23)
+			.text("jour du mois");
+			
+			svg
+			.append("text")
+			.attr("class", "y label")
+			.attr("text-anchor", "end")
+			.attr("y", 25)
+			.attr("dy", ".75em")
+			.attr("transform", "rotate(-90)")
+			.text("nombre d'amis");
+			
+			svg
+			.append('g')
+			.attr('transform', "translate(25," + (h-20) + ")")
+			.call(xAxis);
+			
+			svg
+			.append('g')
+			.attr('transform', "translate(25,10)")
+			.call(yAxis);
+		});
+	}
+	
+	function generatePopularity(url) { 
+		getRequest(url, function(dataset) {
+			var xScale = d3.scale.linear()
+				.domain([0, 35])
+				.range([0, w]);
+				
+			var yScale = d3.scale.linear()
+				.domain([0, 5])
+				.range([h-24, 0]);
+			
+			var xAxis = d3.svg.axis()
+				.scale(xScale)
+				.orient('bottom');
+						
+			var yAxis = d3.svg.axis()
+				.scale(yScale)
+				.orient('left');
+				
+			d3.select("svg")
+			.selectAll("svg")
+			.data(dataset)
+			.enter()	
+			.append("circle")
+			.attr('cx', function(d){ return xScale(parseInt(d[3].substring(8,11))+1);})
+			.attr('cy', function(d){ return yScale(d[2]);})
+			.attr('r', 5)
+			.style("fill", "red");
+			
+			d3.select("svg")
+			.selectAll("svg")
+			.data(dataset)
+			.enter()	
+			.append("text")
+			.text(function(d){
+				return d[2] + ', ' + d[3];
+			})
+			.attr('x', function(d){ return xScale(parseInt(d[3].substring(8,11))+0.5);})
+			.attr('y', function(d){ return yScale(d[2]-0.3);})
+			.style("fill", "red");
+			
+			d3.select("svg")
+			.append("text")
+			.attr("class", "y label")
+			.attr("text-anchor", "end")
+			.attr("y", 750)
+			.attr("dy", ".75em")
+			.attr("transform", "rotate(-90)")
+			.text("popularité")
+			.style("fill", "red");
+			
+			d3.select("svg")
+			.append('g')
+			.attr('transform', "translate(800,10)")
+			.call(yAxis)
+			.style("fill", "red");
+		});	
+	}
+	
+	/***************************************
+		QUESTION 1
+	****************************************/
+	generateListeAmis("webservices/liste_amis_user?user="+user, "listeAmis");
+	generatePopularity("webservices/notations_user?user="+user);
+})
